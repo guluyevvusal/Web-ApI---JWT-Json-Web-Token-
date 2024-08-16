da53417117f5bc3b24c9419290a91af9795b46d7
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-16 21:21:46 +0400

    Security folder created

---
+++ Web Api - JWT/Security/TokenHandler.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Web_Api___JWT.Security
+{
+    public class TokenHandler
+    {
+        public static Token CreateToken (IConfiguration configuration)
+        {
+            Token token = new Token (); 
+            SymmetricSecurityKey securitykey = new SymmetricSecurityKey
+                (Encoding.UTF8.GetBytes(configuration["Token:SecurityKey"]));
+
+            SigningCredentials credentials = new SigningCredentials(securitykey,
+                SecurityAlgorithms.HmacSha256);
+
+            token.Expiration = DateTime.Now.AddMinutes( Convert.ToInt16(configuration["Token:Expiration"]));
+
+            JwtSecurityToken jwtsecuritytoken = new JwtSecurityToken(
+
+                issuer: configuration["Token:Issuer"],
+                audience: configuration["Token:Audience"],
+                expires: token.Expiration,
+                notBefore: DateTime.Now,
+                signingCredentials: credentials
+
+
+
+                );
+
+
+            JwtSecurityTokenHandler tokenHandler = new();
+            token.AccessToken = tokenHandler.WriteToken (jwtsecuritytoken);
+
+            byte[] numbers = new byte[32];
+            using RandomNumberGenerator random =  RandomNumberGenerator .Create ();
+            random.GetBytes (numbers);
+            token.RefreshToken = Convert.ToBase64String (numbers);
+
+            return token;
+        }
+    }
+}
